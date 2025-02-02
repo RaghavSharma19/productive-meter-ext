2de6bbe086fd4fc1c1edcbfaa33b0a079c7a4e35
Raghav <raghav.trunks@gmail.com>
2025-02-03 00:59:52 +0530

    created sample data hours

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import { useState } from 'react'
-
+import data1 from './data.json'; // Correct way to import JSON
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    
     <>
-    <text>intial setup</text>
+     
+      {data1.webpageData.map((element, index) => (
+        <div key={index}>
+          <p>{element.title} - {(element.duration/3600).toFixed(2)} hours </p>
+        </div>
+      ))}
+    
     </>
-  )
+  );
 }
 
 export default App
